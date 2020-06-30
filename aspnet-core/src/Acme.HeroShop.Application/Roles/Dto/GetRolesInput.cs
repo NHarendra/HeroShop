69ef5395d88d91f01c411a120f8859b627e94875
NHarendra <harendra@innolat.com>
2020-06-30 10:56:46 +0530

    heroshop 1st commit
    heroshop 1st commit

---
+++ aspnet-core/src/Acme.HeroShop.Application/Roles/Dto/GetRolesInput.cs
@@ -0,0 +1,7 @@
+﻿namespace Acme.HeroShop.Roles.Dto
+{
+    public class GetRolesInput
+    {
+        public string Permission { get; set; }
+    }
+}
---
+++ aspnet-core/src/Acme.HeroShop.Web.Core/HeroShopWebCoreModule.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Abp.AspNetCore;
+using Abp.AspNetCore.Configuration;
+using Abp.AspNetCore.SignalR;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Abp.Zero.Configuration;
+using Acme.HeroShop.Authentication.JwtBearer;
+using Acme.HeroShop.Configuration;
+using Acme.HeroShop.EntityFrameworkCore;
+
+namespace Acme.HeroShop
+{
+    [DependsOn(
+         typeof(HeroShopApplicationModule),
+         typeof(HeroShopEntityFrameworkModule),
+         typeof(AbpAspNetCoreModule)
+        ,typeof(AbpAspNetCoreSignalRModule)
+     )]
+    public class HeroShopWebCoreModule : AbpModule
+    {
+        private readonly IHostingEnvironment _env;
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public HeroShopWebCoreModule(IHostingEnvironment env)
+        {
+            _env = env;
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        public override void PreInitialize()
+        {
+            Configuration.DefaultNameOrConnectionString = _appConfiguration.GetConnectionString(
+                HeroShopConsts.ConnectionStringName
+            );
+
+            // Use database for language management
+            Configuration.Modules.Zero().LanguageManagement.EnableDbLocalization();
+
+            Configuration.Modules.AbpAspNetCore()
+                 .CreateControllersForAppServices(
+                     typeof(HeroShopApplicationModule).GetAssembly()
+                 );
+
+            ConfigureTokenAuth();
+        }
+
+        private void ConfigureTokenAuth()
+        {
+            IocManager.Register<TokenAuthConfiguration>();
+            var tokenAuthConfig = IocManager.Resolve<TokenAuthConfiguration>();
+
+            tokenAuthConfig.SecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appConfiguration["Authentication:JwtBearer:SecurityKey"]));
+            tokenAuthConfig.Issuer = _appConfiguration["Authentication:JwtBearer:Issuer"];
+            tokenAuthConfig.Audience = _appConfiguration["Authentication:JwtBearer:Audience"];
+            tokenAuthConfig.SigningCredentials = new SigningCredentials(tokenAuthConfig.SecurityKey, SecurityAlgorithms.HmacSha256);
+            tokenAuthConfig.Expiration = TimeSpan.FromDays(1);
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(HeroShopWebCoreModule).GetAssembly());
+        }
+    }
+}
